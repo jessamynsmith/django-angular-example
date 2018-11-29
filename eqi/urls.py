8f01241802fac68c8b2b01f958c31d9f9cbd4b67
jessamynsmith <jessamyn.smith@gmail.com>
2018-11-29 00:19:38 -0700

    Added an example endpoint that sets a custom error response.

--- eqi/urls.py
+++ eqi/urls.py
@@ -6,6 +6,8 @@ from eqi import views
 
 
 router = routers.DefaultRouter()
+# Must register sub-route before higher-level route
+router.register(r'counties/admin', views.CountyAdminViewSet, base_name='counties-admin')
 router.register(r'counties', views.CountyViewSet, base_name='counties')
 
 
--- eqi/views.py
+++ eqi/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from eqi import models as eqi_models
@@ -7,3 +9,15 @@ from eqi import serializers as eqi_serializers
 class CountyViewSet(ModelViewSet):
     queryset = eqi_models.County.objects.all()
     serializer_class = eqi_serializers.CountySerializer
+
+
+class CountyAdminViewSet(ModelViewSet):
+    queryset = eqi_models.County.objects.all()
+    serializer_class = eqi_serializers.CountySerializer
+
+    def list(self, request, *args, **kwargs):
+        user_has_admin = request.user.groups.filter(name='admin')
+        if not user_has_admin:
+            return Response({'invalid': 'Invalid User'}, status=status.HTTP_409_CONFLICT)
+
+        return super().list(request, *args, **kwargs)
